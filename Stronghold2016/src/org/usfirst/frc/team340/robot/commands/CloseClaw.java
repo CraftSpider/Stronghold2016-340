3787227695bb30e1a6658da5187a3dbde93321f3
ashley.m <ashley.m@email.com>
2016-01-21 19:53:36 -0500

    trying something with git

--- Stronghold2016/src/org/usfirst/frc/team340/robot/commands/CloseClaw.java
+++ Stronghold2016/src/org/usfirst/frc/team340/robot/commands/CloseClaw.java
@@ -16,6 +16,7 @@ public class CloseClaw extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	//this is a change 
     }
 
     // Called repeatedly when this Command is scheduled to run
