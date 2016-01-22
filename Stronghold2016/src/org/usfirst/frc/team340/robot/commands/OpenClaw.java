8ef3ffef0f2167d17445f9d2f3a8ae9fba49f28b
wundrweapon <whhacker.dcx@gmail.com>
2016-01-21 20:06:21 -0500

    Added functionality

--- Stronghold2016/src/org/usfirst/frc/team340/robot/commands/OpenClaw.java
+++ Stronghold2016/src/org/usfirst/frc/team340/robot/commands/OpenClaw.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team340.robot.commands;
 
+import org.usfirst.frc.team340.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -18,11 +20,12 @@ public class OpenClaw extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.clawArm.openClaw();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
