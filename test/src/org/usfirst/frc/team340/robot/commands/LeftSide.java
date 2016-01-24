3e54c1e778dcc408c8a5e44c0707df940d82c55a
wundrweapon <whhacker.dcx@gmail.com>
2016-01-23 19:49:53 -0500

    Testing functionality of timeout

--- test/src/org/usfirst/frc/team340/robot/commands/LeftSide.java
+++ test/src/org/usfirst/frc/team340/robot/commands/LeftSide.java
@@ -27,7 +27,7 @@ public class LeftSide extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return isTimedOut();
+        return false;
     }
 
     // Called once after isFinished returns true
--- test/src/org/usfirst/frc/team340/robot/commands/RightSide.java
+++ test/src/org/usfirst/frc/team340/robot/commands/RightSide.java
@@ -27,7 +27,7 @@ public class RightSide extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return isTimedOut();
+        return false;
     }
 
     // Called once after isFinished returns true
