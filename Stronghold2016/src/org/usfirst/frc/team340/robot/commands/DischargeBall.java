3e1305e634a60880df4530602eaed154acba70ae
ashley.m <ashley.m@email.com>
2016-01-30 16:46:20 -0500

    added command to drop the ball

---
+++ Stronghold2016/src/org/usfirst/frc/team340/robot/commands/DischargeBall.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team340.robot.commands;
+
+import org.usfirst.frc.team340.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DischargeBall extends Command {
+
+    public DischargeBall() {
+    	requires(Robot.harvester);
+    	setTimeout(1);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.harvester.setBallControl(.25);
+    	Robot.harvester.setShooter(.2);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.harvester.setBallControl(0);
+    	Robot.harvester.setShooter(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
