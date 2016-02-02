e4c2af78fd6c572e37d93b383ed617b50d93c523
Jakob.w <jakob.w@email.com>
2016-02-01 19:12:30 -0500

    Updated climb to end correctly and not drive forever

--- Stronghold2016/src/org/usfirst/frc/team340/robot/commands/Climb.java
+++ Stronghold2016/src/org/usfirst/frc/team340/robot/commands/Climb.java
@@ -31,17 +31,17 @@ public class Climb extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.climber.isAtBottom();
     }
 
     // Called once after isFinished returns true
     protected void end() {
-//    	Robot.drive.setBothDrive(0, 0);
+    	Robot.drive.setBothDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-//    	end();
+    	end();
     }
 }
