bf8e7ece2e67d036a35e940f34fddd5ad6b03d8e
wundrweapon <whhacker.dcx@gmail.com>
2016-02-15 11:20:06 -0500

    Cheval de frise auto code; changed interrupted() in AutoPortcullis

--- Stronghold2016/src/org/usfirst/frc/team340/robot/commands/auto/AutoChevalDeFrise.java
+++ Stronghold2016/src/org/usfirst/frc/team340/robot/commands/auto/AutoChevalDeFrise.java
@@ -4,19 +4,19 @@ import java.util.logging.Logger;
 
 import org.usfirst.frc.team340.robot.Robot;
 
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class AutoPortcullis extends Command {
-
-	Logger logger = Robot.getLogger(AutoPortcullis.class);
+public class AutoChevalDeFrise extends Command {
+	
+	Logger logger = Robot.getLogger(AutoChevalDeFrise.class);
+	
 	double potAverage;
 	double encAverage;
-	
-    public AutoPortcullis() {
+
+    public AutoChevalDeFrise() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.drive);
@@ -26,32 +26,38 @@ public class AutoPortcullis extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     	logger.info("[Initializing]");
-    	Robot.harvester.resetBothPots();
+    	
     	Robot.drive.resetBothEncoders();
+    	Robot.harvester.resetBothPots();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	encAverage = (Robot.drive.getLeftEncoder() + Robot.drive.getRightEncoder()) / 2;
     	potAverage = (Robot.harvester.getLeftAimPot() + Robot.harvester.getRightAimPot()) / 2;
+    	encAverage = (Robot.drive.getLeftEncoder() + Robot.drive.getRightEncoder()) / 2;
     	
-    	Robot.drive.setBothDrive(0.5, 0.5);
-    	
-    	if(encAverage > 10) {
+    	if(potAverage <= 30) {
     		Robot.harvester.setTilt(0.5);
+    		Robot.drive.setBothDrive(0, 0);
+    	} else if(encAverage <= 500) {
+    		Robot.drive.setBothDrive(0.2, 0.2);
+    	} else if(potAverage > 5) {
+    		Robot.harvester.setTilt(-0.5);
+    	} else {
+    		Robot.drive.setBothDrive(1, 1);
     	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return potAverage >= 110;
+        return encAverage > 2400;
     }
 
     // Called once after isFinished returns true
     protected void end() {
     	logger.info("[Ending]");
-    	Robot.harvester.setTilt(-1);
-    	Robot.drive.setBothDrive(1, 1);
+    	Robot.harvester.setTilt(-0.1);
+    	Robot.drive.setBothDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
