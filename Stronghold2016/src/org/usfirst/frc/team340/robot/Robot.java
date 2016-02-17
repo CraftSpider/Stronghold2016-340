2a0be17cb1c868492deee63e66758b942c241235
wundrweapon <whhacker.dcx@gmail.com>
2016-02-17 13:11:02 -0500

    unnecessary import

--- Stronghold2016/src/org/usfirst/frc/team340/robot/Robot.java
+++ Stronghold2016/src/org/usfirst/frc/team340/robot/Robot.java
@@ -6,7 +6,6 @@ import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 import org.usfirst.frc.team340.robot.subsystems.Climber;
 import org.usfirst.frc.team340.robot.subsystems.Drive;
