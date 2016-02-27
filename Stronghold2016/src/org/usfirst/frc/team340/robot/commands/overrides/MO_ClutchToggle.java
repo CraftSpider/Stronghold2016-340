7a7e7ac4e7ea16acb698e36dd68169b03e0a073d
KyleW <KyleW@email.com>
2016-02-27 11:17:43 -0500

    Actually added StopDrive and MO_ClutchToggle

---
+++ Stronghold2016/src/org/usfirst/frc/team340/robot/commands/overrides/MO_ClutchToggle.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc.team340.robot.commands.overrides;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import java.util.logging.Logger;
+
+import org.usfirst.frc.team340.robot.Robot;
+/**
+ *
+ */
+public class MO_ClutchToggle extends Command {
+	
+	Logger logger = Robot.getLogger(MO_ClutchToggle.class);
+	
+	/**
+	 * Set requirements for clutch operation
+	 * Requires driver subsystem
+	 * Engages the clutch that drives the climbing arm mechanism
+	 */
+    public MO_ClutchToggle() {
+    	
+    	requires(Robot.drive);
+    	
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	logger.info("[Initializing: MO_ClutchOn]");
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    /**
+     * Engages the clutch
+     */
+    protected void execute() {
+    	//latchState default is true which means the climber arm is down
+    	//clutchState default is false which means the clutch is disengaged
+    	if(!Robot.climber.latchState && Robot.drive.clutchState) {
+    		Robot.drive.disengageClutch();
+    	}
+    	else {
+    		Robot.drive.engageClutch();
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    /**
+     * Sets command to completed
+     * @return boolean true
+     */
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
